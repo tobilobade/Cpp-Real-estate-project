ad26d7ede0b3a131161d7d33ff1ae259c3f623f6
EC2 Default User <ec2-user@ip-172-31-27-6.eu-west-1.compute.internal>
2024-02-24 18:44:33 +0000

    new changes to the home page

--- listings/urls.py
+++ listings/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     # Add more URL patterns as needed
 ]
+
+urlpatterns += staticfiles_urlpatterns()
