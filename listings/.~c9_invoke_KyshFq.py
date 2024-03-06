a8e172e5ccd5a364ad6c03b1f15362c0d81e27b2
EC2 Default User <ec2-user@ip-172-31-27-6.eu-west-1.compute.internal>
2024-03-06 17:38:43 +0000

    addition of newsletter

--- listings/.~c9_invoke_KyshFq.py
+++ listings/.~c9_invoke_KyshFq.py
@@ -5,7 +5,6 @@ from .forms import HouseForm
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
-from django.conf import settings
 
 # Create your views here.
 # def homepage(request):
@@ -67,6 +66,42 @@ def delete_house(request, house_id):
         return redirect('sell_house')  # Redirect to homepage after successful deletion
     return render(request, 'listings/sell_house.html', {'house_to_delete': house})
     
+# def subscribe_to_newsletter(request):
+#     if request.method == 'POST':
+#         # Get email address from the form
+#         email = request.POST.get('email')
+        
+#         # Subscribe the user to the SNS topic
+#         sns_client = boto3.client('sns', region_name='eu-west-1')
+#         topic_arn = 'arn:aws:sns:eu-west-1:250738637992:x23212365-Real-estate'
+#         subscription = sns_client.subscribe(
+#             TopicArn=topic_arn,
+#             Protocol='email',
+#             Endpoint=email
+#         )
+        
+#         # Send confirmation message
+#         if subscription['ResponseMetadata']['HTTPStatusCode'] == 200:
+#             message = """
+#             Hi there,
+
+#             Thank you for subscribing to our newsletter! You'll now receive the latest updates and news about our real estate listings.
+
+#             Best regards,
+#             The Modak Estate Team
+#             """            
+#             sns_client.publish(
+#                 TopicArn=topic_arn,
+#                 Message=message,
+#                 Subject='Newsletter Subscription Confirmation'
+#             )
+#             return render(request, 'listings/subscription_success.html')
+#         else:
+#             # Handle subscription failure
+#             return render(request, 'listings/subscription_failure.html')
+#     else:
+#         # Handle GET request (render homepage with subscription form)
+#         return render(request, 'listings/property_homepage.html')
 
 def subscribe_to_newsletter(request):
     if request.method == 'POST':
@@ -83,20 +118,20 @@ def subscribe_to_newsletter(request):
         send_mail(
             subject,
             message,
-            settings.DEFAULT_FROM_EMAIL,  # Combine sender's name with default email address
-            [email],
+            sender=f'{sender_name} <{settings.DEFAULT_FROM_EMAIL}>',  # Combine sender's name with default email address
+            recipient_list=[email],
             fail_silently=False,
             html_message=message
         )
 
-        # # Subscribe the user to the SNS topic
-        # sns_client = boto3.client('sns', region_name='eu-west-1')
-        # topic_arn = 'arn:aws:sns:eu-west-1:250738637992:x23212365-Real-estate'
-        # sns_client.subscribe(
-        #     TopicArn=topic_arn,
-        #     Protocol='email',
-        #     Endpoint=email
-        # )
+        # Subscribe the user to the SNS topic
+        sns_client = boto3.client('sns', region_name='eu-west-1')
+        topic_arn = 'arn:aws:sns:eu-west-1:250738637992:x23212365-Real-estate'
+        sns_client.subscribe(
+            TopicArn=topic_arn,
+            Protocol='email',
+            Endpoint=email
+        )
 
         return render(request, 'listings/subscription_success.html')
     else:
